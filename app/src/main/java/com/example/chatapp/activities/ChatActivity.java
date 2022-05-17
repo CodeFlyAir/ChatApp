c91792f995186a6a012b9dc3805c5aa74aae4ce9
CodeFlyAir <95049059+CodeFlyAir@users.noreply.github.com>
2022-05-17 23:21:53 +0530

    New Chat Feature Completed

--- app/src/main/java/com/example/chatapp/activities/ChatActivity.java
+++ app/src/main/java/com/example/chatapp/activities/ChatActivity.java
@@ -6,15 +6,31 @@ import android.os.Bundle;
 
 import com.example.chatapp.R;
 import com.example.chatapp.databinding.ActivityChatBinding;
+import com.example.chatapp.models.Users;
+import com.example.chatapp.utilities.Constants;
 
 public class ChatActivity extends AppCompatActivity
 {
     private ActivityChatBinding binding;
+    private Users receiverUser;
     @Override
     protected void onCreate (Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         binding=ActivityChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setListeners();
+        loadReceiverDetails();
+    }
+    
+    private void loadReceiverDetails()
+    {
+        receiverUser=(Users) getIntent().getSerializableExtra(Constants.KEY_USER);
+        binding.textName.setText(receiverUser.name);
+    }
+    
+    private void setListeners()
+    {
+        binding.imageBack.setOnClickListener(v -> onBackPressed());
     }
 }
